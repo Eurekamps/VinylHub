66cbe4e2e83fec8896c846c2b193878b6a6f6f8f
Carlos <carlosgomezr77@gmail.com>
2024-11-28 18:56:46 +0100

    resolucion conflicto

--- lib/Views/LoginView.dart
+++ lib/Views/LoginView.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../../FbObjects/FbPerfil.dart';
 import 'package:hijos_de_fluttarkia/Singletone/DataHolder.dart';
 
 import '../CustomViews/VinylBoton.dart';
